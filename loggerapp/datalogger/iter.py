cf2a82e02b5658234e657a8e8f2a27facd90594a
martinlink00 <69633819+martinlink00@users.noreply.github.com>
2020-08-24 17:36:05 +0200

    Snapshot trigger type was enabled

--- loggerapp/datalogger/iter.py
+++ loggerapp/datalogger/iter.py
@@ -22,3 +22,15 @@ def cycle(sensors,client):
                     log.info("Sensor %s of type %s has exported data. This took %f seconds." % (sensor.sensor,sensor.type,endtime-starttime))
                 else:
                     log.warning("Sensor %s of type %s could not export data" % (sensor.sensor,sensor.type))
+            
+            if sensor.snapshot:
+                starttime=time()
+                sensordata=sensor.exporttoinflux()
+                if sensordata is not None:
+                    client.write_points(sensordata)
+                    endtime=time()
+                    log.info("Sensor %s of type %s has exported data. This took %f seconds." % (sensor.sensor,sensor.type,endtime-starttime))
+                else:
+                    log.warning("Sensor %s of type %s could not export data" % (sensor.sensor,sensor.type))
+                
+                sensor.snapshot=False
