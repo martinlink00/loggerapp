af257021572aa2a3a0e84d7a4b4046084ff8d0ec
martinlink00 <69633819+martinlink00@users.noreply.github.com>
2020-08-31 16:03:36 +0200

    Snapshot feature changed

--- loggerapp/datalogger/iter.py
+++ loggerapp/datalogger/iter.py
@@ -23,14 +23,15 @@ def cycle(sensors,client):
                 else:
                     log.warning("Sensor %s of type %s could not export data" % (sensor.sensor,sensor.type))
             
-            if sensor.snapshot:
-                starttime=time()
-                sensordata=sensor.exporttoinflux()
-                if sensordata is not None:
-                    client.write_points(sensordata)
-                    endtime=time()
-                    log.info("Sensor %s of type %s has exported data via a snapshot. This took %f seconds." % (sensor.sensor,sensor.type,endtime-starttime))
-                else:
-                    log.warning("Sensor %s of type %s could not export data" % (sensor.sensor,sensor.type))
-                
-                sensor.snapshot=False
+
+            
+            
+def snapshot(sensor,client):
+    starttime=time()
+    sensordata=sensor.exporttoinflux()
+    if sensordata is not None:
+        client.write_points(sensordata)
+        endtime=time()
+        log.info("Sensor %s of type %s has exported data via a snapshot. This took %f seconds." % (sensor.sensor,sensor.type,endtime-starttime))
+    else:
+        log.warning("Sensor %s of type %s could not export data" % (sensor.sensor,sensor.type))
